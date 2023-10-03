8e280c21704cbb59c038e5a6f4393998b5444c61
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2023-10-03 12:54:45 +0200

    extremely crude crap

---
+++ VideoCrash/Program.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Video;
+using osu.Framework.IO.Stores;
+using osu.Framework.Platform;
+
+namespace VideoCrash
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            using (GameHost host = Host.GetSuitableDesktopHost(@"sample-game"))
+            using (Game game = new SampleGameGame())
+                host.Run(game);
+        }
+    }
+
+    public partial class SampleGameGame : Game
+    {
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            Resources.AddStore(new NamespacedResourceStore<byte[]>(new DllResourceStore(typeof(SampleGameGame).Assembly), "Resources"));
+            var store = new NamespacedResourceStore<byte[]>(Resources, @"Videos");
+
+            Add(new Video(store.GetStream(@"h264.avi"))
+            {
+                RelativeSizeAxes = Axes.Both
+            });
+
+            Scheduler.AddDelayed(Exit, 5000);
+        }
+    }
+}
